0b3089817b05631df8876b9fb654954e39477963
harshal joshi <harshel_84@msn.com>
2015-09-16 20:25:14 -0400

    Initial Checking - BowlingApp Project

---
+++ BowlingApp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BowlingAppDBData;
+
+namespace BowlingApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var x = new BowlingDBStoreEntities();
+
+            x.Users.Add(new User { FirstName = "A", LastName = "B" });
+            x.SaveChanges();
+
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ BownlingAppDB/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[UserId] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NCHAR(10) NULL, 
+    [LastName] NCHAR(10) NULL, 
+    [EmailAddress] NCHAR(50) NULL, 
+    [Password] NCHAR(100) NULL
+)
